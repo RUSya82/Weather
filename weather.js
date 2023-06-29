2a99446343405def3cfa1381cf2cbc5c3aa8c0de
RuslanSalimov <mail-spam2012@yandex.ru>
2023-06-29 15:01:20 +0500

    create save token

--- weather.js
+++ weather.js
@@ -1,8 +1,20 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
 import {printSuccess, printError, printHelp} from './services/log.service.js';
-import {saveKeyValue} from "./services/storage.service.js";
+import {saveKeyValue, getKeyValue} from "./services/storage.service.js";
 
+const saveToken = async (token) => {
+    if (!token.length) {
+        printError('Не передан token');
+        return;
+    }
+    try {
+        await  saveKeyValue('token', token);
+        printSuccess('Token saved');
+    } catch (e){
+        printError(e.message);
+    }
+}
 const initCLI = () => {
     const args = getArgs(process.argv);
     if(args.s){
@@ -12,7 +24,7 @@ const initCLI = () => {
         printHelp();
     }
     if(args.t){
-        saveKeyValue('token', args.t)
+        return saveToken(args.t)
     }
 }
 initCLI();
